5aaddbb0aed43b935f014646b19c802735309d9e
Maya Inkova <139772324+MayaInkova@users.noreply.github.com>
2024-11-09 17:01:12 +0200

    String

--- src/Loops/EvenPairs.java
+++ src/Loops/EvenPairs.java
@@ -31,12 +31,12 @@ public class EvenPairs { // Функция за проверка дали чис
         System.out.print("Въведете разликата за втората двойка (1-9): ");
         int diff2 = scanner.nextInt();
 
-        // Генериране на четирицифрени числа
+        // Генериране на четирицифрените числа
         for (int i = initialPair1; i <= initialPair1 + diff1; i++) {
             for (int j = initialPair2; j <= initialPair2 + diff2; j++) {
                 // Проверка дали и двете числа са прости
                 if (isPrime(i) && isPrime(j)) {
-                    // Отпечатване на четирицифреното число
+
                     System.out.println("" + i + j);
                 }
             }
