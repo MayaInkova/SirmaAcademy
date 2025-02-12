93fb0b84f1f662ef6b40e8c2a2555edade0dd48c
Maya Inkova <Maq002@abv.bg>
2025-02-12 14:42:02 +0200

    Edit

---
+++ src/HashMap.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class HashMap {
+    public static void main(String[] args) {
+        String text = " Hello, world!";
+
+        // Премахваме пунктуациите и правим всички букви малки
+        text = text.replaceAll("[^a-zA-Z ]", "").toLowerCase();
+
+        // разделяме текста на думи
+        String[] words = text.split("\\s+");
+
+        //Използваме HashMap за да преброим думите
+        Map<String, Integer> wordCountMap = new java.util.HashMap<>();
+        for (String word : words) {
+            wordCountMap.put(word, wordCountMap.getOrDefault(word, 0) + 1);
+
+        }
+        //Сортиране по стойността на мапа
+        List<Map.Entry<String, Integer>> sortedEntries = new ArrayList<>(wordCountMap.entrySet());
+        sortedEntries.sort((entry1,entry2) -> entry2.getValue().compareTo(entry1.getValue()));
+
+
+        for (Map.Entry<String, Integer> entry : sortedEntries) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+}
+
